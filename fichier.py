e21714a2bac690f139aecb0f71135252b1d4219c
T0ma-cl <165392531+T0ma-cl@users.noreply.github.com>
2024-05-19 18:12:26 +0200

    Update fichier.py

--- fichier.py
+++ fichier.py
@@ -1,14 +1,20 @@
+import pickle
+
 class Fichier :
     def __init__(self,nom_fichier):
-        self.nf=nom_fichier
+        self.nom_fichier = nom_fichier
+   
     def ecrire (self,data):
-        with open(self.nf,'w') as f :
-            f.write(data)
+        fichier = open(self.nom_fichier, "wb")
+        pickle.dump(data, fichier)
+        fichier.close()
+        
+    
     def lire (self,data):
-        with open(self.nf,'r') as g :
-            data=g.read()
-            return data
-
-            
+        fichier = open(self.nom_fichier, "rb")
+        data = pickle.load(fichier)
+        fichier.close()
+        return data
+        
         
 
